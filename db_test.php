f962003fe393d58be87d284656341633448bc6d6
straysheep <straysheep1461@gmail.com>
2014-07-19 11:16:51 +0900

    Modified: mysql_connect() function -> mysqli_connect()

--- db_test.php
+++ db_test.php
@@ -1,8 +1,8 @@
-<!--?php <br ?--> $link = mysql_connect('localhost', 'root', '');
-if (!$link) {
-die('接続失敗！
-'.mysql_error());
-} else {
-echo '接続成功！';
-}
-?&gt;
+<?php
+ $link = mysqli_connect('localhost', 'root', '');
+ if (!$link) {
+ die('接続失敗！<br>'.mysqli_connect_error());
+ } else {
+ echo '接続成功！';
+ }
+?>
